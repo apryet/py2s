b32cbd88781da745db6eb342101b45e83874b088
Kenny <christian_dom_1986@hotmail.com>
2014-05-23 03:55:57 -0500

    time step optimisation
    Mass balance and flux still in progress

--- pysoil/EjemploHydrus.py
+++ pysoil/EjemploHydrus.py
@@ -265,7 +265,7 @@ soil_carac = {'Ksat':1e-4, 'Ss':0, 'eta':0.368, 'theta_r' : 0.102, 'theta_s' : 0
 
 T = 3600. # total simulation duration [s] 
 L = 0.7 # model length [m]
-dt = 10 # time step [s]
+dt = 1 # time step [s]
 dz = 0.01 # mesh cell size [m]
 
 N = int(round(T/dt)) # number of time steps
@@ -301,4 +301,169 @@ import csv
 with open('/Users/redhotkenny/Documents/Documentos/Doctorado/SoilModel/compHydrus/test.csv', 'wb') as test_file:
     AA = csv.writer(test_file, delimiter=',')
     AA.writerow(np.asarray(S)[:,N]) 
+
+
+
+
+
+
+
+# Check all values below
+
+# Soil characteristics 
+soil_carac = {'Ksat':1e-4, 'Ss':0, 'eta':0.368, 'theta_r' : 0.102, 'theta_s' : 0.368, 'n':2, 'alpha':3.35, 'FC1':0.2, 'FC2':0.25}
+
+# time and space discretization
+
+T = 88. # total simulation duration [s] 
+L = 0.7 # model length [m]
+dt = 1. # time step [s]
+dz = 0.01 # mesh cell size [m]
+
+N = int(round(T/dt)) # number of time steps
+I = int(round(L/dz)) # number of cells
+
+#Root Information
+LR=0.5
+type='distribution'
+#get Root Distribution
+z = np.linspace(0,L,I)
+RD = get_RD(L, I, LR, z, dz, type) 
+
+
+# boundary conditions
+h_bot=-0.97
+#h_bot = -0.00001/3600
+h_top = -0.001/3600
+bc = {'top':['fixed_flow',[h_top]*N],'bot':['free_drainage']}
+#bc = {'top':['fixed_flow',[h_top]*N],'bot':['fixed_flow',[h_bot]*N]}
+#bc = {'top':['fixed_flow',[h_top]*N],'bot':['fixed_head',[h_bot]*N]}
+
+# source term
+q = 0
+
+# initial condition
+h_init = np.array([-0.97]*I)
+
+# --------------------- Simulation run --------------------------
+t, z, S, INF, RO = run_varsat(L, T, dz, dt, h_init, bc, q, soil_carac)
+
+
+
+
+#plt.plot(np.asarray(S)[:,N],z)
+plt.plot(100.*np.asarray(S)[:,N],100.*z)
+
+
+import csv
+with open('/Users/redhotkenny/Documents/Documentos/Doctorado/SoilModel/compHydrus/test.csv', 'wb') as test_file:
+    AA = csv.writer(test_file, delimiter=',')
+    AA.writerow(100.*np.asarray(S)[:,N]) 
+
+
+
+
+
+soil_carac = {'Ksat':1e-2, 'Ss':0, 'eta':0.368, 'theta_r' : 0.102, 'theta_s' : 0.368, 'n':2, 'alpha':0.0335, 'FC1':0.2, 'FC2':0.25}
+
+# time and space discretization
+
+T = 3600. # total simulation duration [s] 
+L = 70 # model length [cm]
+dt = 1 # time step [s]
+dz = 1 # mesh cell size [cm]
+
+N = int(round(T/dt)) # number of time steps
+I = int(round(L/dz)) # number of cells
+
+#Root Information
+LR=0.5
+type='distribution'
+#get Root Distribution
+z = np.linspace(0,L,I)
+RD = get_RD(L, I, LR, z, dz, type) 
+
+
+# boundary conditions
+h_bot = -0.001/3600
+h_top = -0.1/3600
+bc = {'top':['fixed_flow',[h_top]*N],'bot':['free_drainage']}
+
+# source term
+q = 0
+
+# initial condition
+h_init = np.array([-97.]*I)
+
+# --------------------- Simulation run --------------------------
+t, z, S, INF, RO = run_varsat(L, T, dz, dt, h_init, bc, q, soil_carac)
+
+
+
+
+plt.plot(np.asarray(S)[:,N],z)
+
+
+import csv
+with open('/Users/redhotkenny/Documents/Documentos/Doctorado/SoilModel/compHydrus/test.csv', 'wb') as test_file:
+    AA = csv.writer(test_file, delimiter=',')
+    AA.writerow(np.asarray(S)[:,N]) 
+
+
+
+
+
+
+
+# Soil characteristics 
+soil_carac = {'Ksat':1e-4, 'Ss':0, 'eta':0.368, 'theta_r' : 0.102, 'theta_s' : 0.368, 'n':2, 'alpha':3.35, 'FC1':0.2, 'FC2':0.25}
+
+# time and space discretization
+
+T = 3600. # total simulation duration [s] 
+timestep=np.array([0.,600.,1200.,1800.,2400.,3000.])
+tstep=1.
+L = 0.70 # model length [m]
+#dt = 60 # time step [s]
+dz = 0.01 # mesh cell size [m]
+I = int(round(L/dz)+1) # number of cells
+
+#Root Information
+LR=0.5
+type='distribution'
+#get Root Distribution
+z = np.linspace(0,L,I)
+RD = get_RD(L, I, LR, z, dz, type) 
+
+
+# boundary conditions
+h_bot=-0.97
+#h_bot = -0.00001/3600
+h_top = -0.001/3600
+bc = {'time':timestep,'top':['fixed_flow',[h_top]*len(timestep)],'bot':['free_drainage']}
+#bc = {'top':['fixed_flow',[h_top]*N],'bot':['fixed_flow',[h_bot]*N]}
+#bc = {'top':['fixed_flow',[h_top]*N],'bot':['fixed_head',[h_bot]*N]}
+
+# source term
+q = np.array([0,0,0,0,0,0])
+
+# initial condition
+h_init = np.array([-0.97]*I)
+
+# --------------------- Simulation run --------------------------
+t, z, S, INF, RO, Theta = run_varsat(L, T, dz, tstep, h_init, bc, q, soil_carac)
+
+
+
+
+#plt.plot(np.asarray(S)[:,N],z)
+plt.plot(100.*np.asarray(S)[:,-1],100.*z)
+
+
+import csv
+with open('/Users/redhotkenny/Documents/Documentos/Doctorado/SoilModel/compHydrus/test.csv', 'wb') as test_file:
+    AA = csv.writer(test_file, delimiter=',')
+    AA.writerow(100.*np.asarray(S)[:,(len(t)-1)]) 
+
+
 
--- pysoil/varsat_1D.py
+++ pysoil/varsat_1D.py
@@ -22,6 +22,7 @@ from scipy.sparse import csr_matrix
 # -------------------- Declare functions ------------------------
 # --------------------------------------------------------------
 
+
 # -- Van Genuchten functions
 
 def theta_curve(h,soil_carac):
@@ -173,12 +174,12 @@ def get_b2(i,theta,soil_carac):
 # coefficient b3 (g in Clement et al., 1994)
 def get_b3(i,Kp):
     #if i == 0 : 
-	return(-(Kp[i+1]-Kp[i])/dz)
+    return(-(Kp[i+1]-Kp[i])/dz)
     #return( 0 )
         #return( -( K[i+1]-K[i] ) / (2.*dz) )
-	#return( -( K[i+2]-K[i] ) / (2.*dz) )
+    #return( -( K[i+2]-K[i] ) / (2.*dz) )
     #else :     
-	#return( -( K[i+1]-K[i-1] ) / (2.*dz) )
+    #return( -( K[i+1]-K[i-1] ) / (2.*dz) )
 
 
 # coefficient b4 (h in Clement et al., 1994)
@@ -198,12 +199,12 @@ def w_uptake(theta0, RD, q, soil_carac):  #from Feddes et al., 1978
         elif theta0[i]<soil_carac['theta_r']:
             red=0
         else:
-            WU[i]=red*RD[i]*q[i,n]
+            WU[i]=red*RD[i]*q[ww]*I/(I-1)
     return(WU)
 
 
 # build system of equation
-def build_system(h0,h,theta0, bc, q, soil_carac, n, bcff):
+def build_system(h0,h,theta0, bc, q, soil_carac, ww, bcff):
     # init vectors
     theta = get_theta(h,soil_carac)
     C     = get_C(h,soil_carac)
@@ -225,12 +226,12 @@ def build_system(h0,h,theta0, bc, q, soil_carac, n, bcff):
     if bc['top'][0] == 'fixed_head':
        M[-1,-1] = 1. 
        M[-1,-2] = 0.  
-       B[-1]   = bc['top'][1][n]
+       B[-1]   = bc['top'][1][ww]
     # bot
     if bc['bot'][0] == 'fixed_head':
        M[0,0] = 1. 
        M[0,1] = 0.  
-       B[0]   = bc['bot'][1][n]
+       B[0]   = bc['bot'][1][ww]
     # fixed flow
     # top
     if bc['top'][0] == 'fixed_flow':
@@ -242,7 +243,7 @@ def build_system(h0,h,theta0, bc, q, soil_carac, n, bcff):
                 M[-1,-2] = get_m1(I-1,Kp)
                 M[-1,-1] = get_m2(I-1,Kp,C,theta) + get_m3(I-1,Kp)
                 B[-1] = - get_b1(I-1,C)*h[-1] - get_b2(I-1,theta,soil_carac)*h0[-1] \
-                + get_b3(I-2,Kp) + get_b4(I-1,theta0,theta) + dz*WU[I-1] + get_m3(I-1,Kp)*dz*(bc['top'][1][n]/Kp[-1] +1)
+                + get_b3(I-2,Kp) + get_b4(I-1,theta0,theta) + WU[I-1] + get_m3(I-1,Kp)*dz*(bc['top'][1][ww]/Kp[-1] +1)
         #change get_m1(I-1,Kp)*dz*(bc['top'][1][n]/Kp[-1] +1) by get_m3(I-1,Kp)*dz*(bc['top'][1][n]/Kp[-1] +1)
         # bot
         #if bc['bot'][0] == 'fixed_flow':
@@ -255,7 +256,7 @@ def build_system(h0,h,theta0, bc, q, soil_carac, n, bcff):
         M[0,1] = get_m3(0,Kp)
         M[0,0] = get_m2(0,Kp,C,theta) + get_m1(0,Kp)
         B[0] = - get_b1(0,C)*h[0] - get_b2(0,theta,soil_carac)*h0[0] \
-        + get_b3(0,Kp) + get_b4(0,theta0,theta) + WU[0] - get_m1(0,Kp)*dz*(bc['bot'][1][n]/Kp[0] +1)
+        + get_b3(0,Kp) + get_b4(0,theta0,theta) + WU[0] - get_m1(0,Kp)*dz*(bc['bot'][1][ww]/Kp[0] +1)
         #Change the values... get_b3 not sure what will result
 
     # free drainage at the bottom
@@ -273,11 +274,15 @@ def build_system(h0,h,theta0, bc, q, soil_carac, n, bcff):
         + get_b3(0,Kp) + get_b4(0,theta0,theta) + WU[0]
      
     # return M and B
-    return(np.mat(M), np.transpose(np.mat(B)) )
+    return(np.mat(M), np.transpose(np.mat(B)), theta, C, K, Kp, WU )
 
+# -- Global dt variable
+def gl_val(dts):
+    global dt
+    dt=dts
 
 # model
-def run_varsat(L, T, dz, dts, h_init, bc, q, soil_carac, PICmax = 1000, CRIT_CONV = 1e-5, Runoff='True'):
+def run_varsat(L, T, dz, tstep, h_init, bc, q, soil_carac, PICmax = 10, CRIT_CONV = 1e-2, Runoff='True'):
     # L : column length
     # T : simulation duration
     # dz : vertical discretization
@@ -293,21 +298,31 @@ def run_varsat(L, T, dz, dts, h_init, bc, q, soil_carac, PICmax = 1000, CRIT_CON
     # Runoff='True' --> Runoff will be calculated instead of a pounding condition
     # -- model initialization
     # init vars
-    if hasattr(dts,"__len__") == False:
-        dts  = np.array( [dts] * int(round(T/dts)) )
-    t = np.hstack((0,np.cumsum(dts))) # time array
-    N = len(dts) # number of time steps
-    I = int(round(L/dz)) # number of cells
+    tmin=1.
+    tmax=3600.
+    if tstep>T:
+        tstep=T
+    if tstep>tmax:
+        tstep=3600.
+    if tstep<tmin:
+        tstep=1.
+
+    #if hasattr(dts,"__len__") == False:
+        #dts  = np.array( [dts] * int(round(T/dts)) )
+    t=np.array(0)
+    #t = np.hstack((0,np.cumsum(dts))) # time array
+    N = int(round(T/tmin)) # number of time steps
+    I = int(round(L/dz)+1) # number of cells
     z = np.linspace(0,L,I) # z coordinates of nodes
     # check h_init
     if len(h_init) != I:
         print('ERROR: check dimension of h_init')
         return(0,0,0)
     #  check q (not fully implemented)
-    if hasattr(q,"__len__") == False: #  constant and homogeneous q
-        q  = np.array( [ [q] * N] * I  )
-    elif np.array(q).shape == (I,) :  # transient homogeneous q
-        q = np.transpose(np.array(  [list(q)]*N ) ) 
+    #if hasattr(q,"__len__") == False: #  constant and homogeneous q
+    #    q  = np.array( [ [q] * N] * I  )
+    #elif np.array(q).shape == (I,) :  # transient homogeneous q
+    #    q = np.transpose(np.array(  [list(q)]*N ) ) 
     # -- check input data
     # check bc : 
     if bc['top'][0] not in ['fixed_head','fixed_flow','free_drainage'] :
@@ -315,56 +330,104 @@ def run_varsat(L, T, dz, dts, h_init, bc, q, soil_carac, PICmax = 1000, CRIT_CON
     if bc['bot'][0] not in ['fixed_head','fixed_flow','free_drainage'] :
         print('ERROR: check bottom boundary condition')
     #initialize Runoff and Infiltration
-    RO = np.zeros((N))
-    INF = bc['top'][1]
+    RO = np.zeros((1))
+    INF = np.zeros((1))
     
     # -- run initization
     # initialize output matrices
-    S=np.mat(np.zeros(shape=(I,N+1))) 
+    S=np.mat(np.zeros(shape=(I,1))) 
     #include the time 0
     # Store initial condition
     S[:,0]= np.transpose(np.mat(h_init))
     h = h0 = h_init 
     h_list = []
     # iterate over time
-    for n in range(0,N):
-        dt = dts[n]
+    dts=tstep
+    dt=dts
+    t=np.zeros([1])
+    for n in range(1,N):
+        t=np.hstack((t,(t[-1]+dts)))
+        for w in range(1,len(bc['time'])):
+            if t[-1]<bc['time'][w]:
+                ww=w-1
+                break
+            else:
+                ww=w
+        gl_val(dts)
         theta0 = get_theta(h0,soil_carac)
+        RO=np.hstack((RO,0.))
+        INF=np.hstack((INF,bc['top'][1][ww]*dt)) #we must choose if we want to calculate the infiltration even with fixed head, now it only calculates to fixed flow
         # Picard iteration 
         h00=h
         for m in range(PICmax):
-            M , B = build_system(h0, h, theta0 , bc, q, soil_carac, n, bcff='False')
+            Iter=m
+            M , B, = build_system(h0, h, theta0 , bc, q, soil_carac, ww, bcff='False')
             # solver linear system
             h1 = np.transpose(np.matrix(cgs(csr_matrix(M),B)[0]))
             if np.max(h1-h) < CRIT_CONV:
-                if Runoff=='True':
-                    if h1[I-1]> 0 :
-                        #Include the Runoff Modification
-                        h=h00
-                        for mm in range (PICmax):
-                            M , B = build_system(h0, h, theta0 , bc, q, soil_carac, n,bcff='True')
-                            # solver linear system
-                            h1 = np.transpose(np.matrix(cg(csr_matrix(M),B)[0])) # see also cgs, csr, spsolve
-                            if np.max(h1-h) < CRIT_CONV:   
-                                print( 'Runoff Modification PIC iteration ='+str(mm))
-                                theta = get_theta(h,soil_carac)
-                                C     = get_C(h,soil_carac)
-                                K     = get_K(h,soil_carac)
-                                Kp    = get_Kp(K)
-                                Infil = Kp[-1]*(((get_m1(I-1,Kp)*h1[-2]+get_b2(I-1,theta,soil_carac)*h0[-1] \
-                                    -get_b3(I-2,Kp)-get_b4(I-1,theta0,theta))/(get_m3(I-1,Kp)*dz))-1)
-                                RO[n] = bc['top'][1][n]-Infil[0,0]
-                                INF[n]=Infil[0,0]
-                                break
-                            h=h1
                 print('PIC iteration = '+ str(m))
-                h = h1
                 break
-            h = h1
+            h=h1
+        if Runoff=='True':
+            if h1[-1]> 0 :
+                #Include the Runoff Modification                        
+                h=h00
+                for mm in range (PICmax):
+                    Iter=mm
+                    M , B = build_system(h0, h, theta0 , bc, q, soil_carac, ww, bcff='True')
+                    # solver linear system
+                    h1 = np.transpose(np.matrix(cg(csr_matrix(M),B)[0])) # see also cgs, csr, spsolve
+                    if np.max(h1-h) < CRIT_CONV:   
+                        print( 'Runoff Modification PIC iteration ='+str(mm))
+                        break
+                    h=h1
+                h=h1
+                Infil = Kp[-1]*(((get_m1(I-1,Kp)*h1[-2]+get_b2(I-1,theta,soil_carac)*h0[-1] \
+                    -get_b3(I-2,Kp)-get_b4(I-1,theta0,theta))/(get_m3(I-1,Kp)*dz))-1)
+                RO=np.hstack((RO, RO[-1] = (bc['top'][1][ww]-Infil[0,0])*dt))
+                INF=np.hstack((INF,Infil[0,0]*dt))      
+        h=h1
         h0 = h
-        S[:,n+1] = np.mat(h)
+        if t[-1]==bc['time'][ww]:
+            dts=dts #key feature, if flux input vary variable it would be better to use dts=tmin
+        else:
+            if t[-1]>bc['time'][(len(bc['time'])-1)]:
+                if Iter<=3:
+                    dts=max(1.3*dts,tmin)
+                    if (T-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(T-t[-1]))
+                elif Iter>3 and Iter<=7:
+                    dts=dts
+                    if (T-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(T-t[-1]))
+                elif Iter>7 and Iter<PICmax:
+                    dts=max(0.7*dts,tmin)
+                    if (T-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(T-t[-1]))
+                else:
+                    dts=max(dts/3,tmin)
+                    if (T-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(T-t[-1]))
+            else:
+                if Iter<=3:
+                    dts=max(1.3*dts,tmin)
+                    if (bc['time'][ww+1]-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(bc['time'][ww+1]-t[-1]))
+                elif Iter>3 and Iter<=7:
+                    dts=dts
+                    if (bc['time'][ww+1]-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(bc['time'][ww+1]-t[-1]))
+                elif Iter>7 and Iter<PICmax:
+                    dts=max(0.7*dts,tmin)
+                    if (bc['time'][ww+1]-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(bc['time'][ww+1]-t[-1]))
+                else:
+                    dts=max(dts/3,tmin)
+                    if (bc['time'][ww+1]-t[-1])<=(dts+tmin):
+                        dts=max(tmin,(bc['time'][ww+1]-t[-1]))
         print('iteration ' + str(n) + ' terminated.')
+        if t[-1]==T:
+            break
     # return simulation results
-    return(t,z,S,INF,RO)
 
-    
+    return(t,z,S,INF,RO)
